6d5f15b434448c1323a9b662c4ff297ced826c96
Yashhh230 <yp7202963@gmail.com>
2024-11-21 20:59:37 +0530

    [yash-auth]=> design implmented

--- GNEC-frontend/src/app/shared/constant/api.constant.ts
+++
@@ -1,8 +0,0 @@
-export const API = {
-    SIGN_UP: 'user/registration',
-    SIGN_IN: 'user/login',
-    CHANGE_PASSWORD: 'user/changePassword',
-  FORGOT_PASSWORD: 'user/forget-password/',
-  CONFIRM_EMAIL:'user/confirm-email',
-  };
-  
--- GNEC-frontend/src/app/shared/constant/toast.ts
+++
@@ -1,5 +0,0 @@
-export const TOAST_TYPE = {
-    success:'success',
-    warning: 'warning',
-    danger:'danger'
-}
